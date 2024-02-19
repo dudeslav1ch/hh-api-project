78bf905ed7fe165735911c3fc4722d8b84224b50
dudeslav1ch <dudeslav1ch@gmail.com>
2024-02-19 19:01:42 +0300

    add doc and fix code

--- src/GetApiHh.py
+++ src/GetApiHh.py
@@ -1,4 +1,6 @@
+import json
 import requests
+
 from abc import ABC, abstractmethod
 
 
@@ -10,12 +12,15 @@ class AbstractGetApiHh(ABC):
 
 
 class GetApiHh(AbstractGetApiHh):
+    """
+    Получает информацию о вакансиях
+    """
     def __init__(self):
-        self.all_vacancy = []
+        self.all_vacancies = []
 
     def get_vacancies(self, keyword):
-        key_response = {'text': f'NAME:{keyword}', 'area': 1, 'per_page': 100}
+        key_response = {'text': f'NAME:{keyword}', 'area': 113, 'per_page': 100}
         url_get = "https://api.hh.ru/vacancies"
         response = requests.get(url_get, key_response)
-        self.all_vacancy = response.json()["items"]
-        return self.all_vacancy
+        self.all_vacancies = json.loads(response.text)['items']
+        return self.all_vacancies
